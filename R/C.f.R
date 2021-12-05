930e94d15a260ad2bfc754efe172964be012958f
Yi Ya <Yi.YA_yaya@hotmail.com>
2021-12-05 10:00:02 +0000

    version 2.0

--- R/C.f.R
+++ R/C.f.R
@@ -1,13 +1,20 @@
-C.f<-function(dat,type) {        
+C.f<-function(dat,type) {    
+   
 	  sq<-vector()
-	  for(i in 1:nrow(dat)) {
+        if(nrow(dat)==1) { 
+            ct<-dat 
+        } else {
+	      for(i in 1:nrow(dat)) {
 		    D<-vector()
 		    for(j in 1:nrow(dat))
-		        D[j]<-Dist(dat[i,],dat[j,],type)$d
-		    sq[i]<-sum(D^2)
-	  }
-	  g<-which(sq==min(sq))[1]
-	  dat[g,]
+		        D[j]<-Dist(dat[i,],dat[j,],type)
+		    sq[i]<-sqrt(sum(D^2))
+	      }
+	      g<-which(sq==min(sq))[1]
+	      ct<-dat[g,]
+       }
+       
+       ct
 }
 
 
--- R/MKMeans.R
+++ R/MKMeans.R
@@ -1,45 +1,68 @@
 MKMeans<-function(data, K, initial, iteration, tol, type) {
 	
-	  if(initial==1) C<-data[1:K,] else C<-initial
-	  cl<-list()
-	  CL<-list()
-	  cl.old<-list()
+	  if(initial==1) {
+            C<-data[1:K,]
+        } else { C<-initial}
+
+	  cl<-list()         # index cluster
+	  CL<-list()         # obs cluster
+        cl.previous<-list()
+        for(i in 1:15) cl.previous[[i]]<-list()
+        cl.old<-list()
 	  it<-0
-	  p1<-p2<-p<-0
-	  for(j in 1:K)  cl.old[[j]]<-vector()
-	
+        p<-0
+
+	  for(j in 1:K)  {
+            cl.old[[j]]<-vector()
+            for(i in 1:15) cl.previous[[i]][[j]]<-vector()
+        }                
+
 	  while(it<iteration&p<tol) {	  	
-		     it<-it+1
+		it<-it+1
 	      for(j in 1:K) cl[[j]]<-vector()    	  
 	      for(i in 1:nrow(data)) {
 	          D<-vector()
 		        for(j in 1:K) 
-			           D[j]<-Dist(data[i,],C[j,],type)$d
-		        t<-which(D==min(D))
+			           D[j]<-Dist(data[i,],C[j,],type)
+		        t<-which(D==min(D))[1]
 		        l<-cl[[t]]
 		        cl[[t]]<-c(l,i)		
 	      }
-	
+
+            S<-0           #the sum of distances (meaning of MSE)
 	      for(j in 1:K) {
 		        CL[[j]]<-data[cl[[j]],]
 		        dat<-CL[[j]]
-           C[j,]<-C.f(dat,type)		
+                    if(length(cl[[j]])==1) dat<-matrix(CL[[j]],nrow=1)    
+                    C[j,]<-C.f(dat,type)	
+                    for(s in cl[[j]]) 
+                             S<-S+Dist(data[s,],C[j,],type)	
 	      }	
+
+
 	      
-	      mat<-0
-	      for(j in 1:K) 
-	         mat<-mat+length(intersect(cl.old[[j]],cl[[j]]))
-	      
-	      p1<-p2
-	      p2<-mat/nrow(data)
-	      p<-min(p1,p2)
-	      cl.old<-cl
-	      cat("Iteration:",it,"-----","Matching1:",p1,"-----","Matching2:",p2,"\n")
+	      mat<-0          
+	      for(j in 1:K) {
+               mat1<-cl.previous[[15]][[j]]
+               for(i in 1:14) {                   
+                    mat1<-intersect(mat1,cl.previous[[15-i]][[j]])
+               }
+               mat<-mat+length(intersect(intersect(mat1,cl.old[[j]]),cl[[j]]))
+            }
+            
+	      p<-mat/nrow(data)
+
+            for(i in 1:14) cl.previous[[i]]<-cl.previous[[i+1]]
+            cl.previous[[15]]<-cl.old
+            cl.old<-cl
+
+            cat("Iteration:",it,"-----","Stable Percentage:",p,"-----","TSD: ",S,"\n")
 	   }
+
+
 	
     	return(new("MKMean",K=K,Centers=C,Classes=cl,Clusters=CL))
 }
-
 
 
 
